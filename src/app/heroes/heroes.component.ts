dc5cd5c78b5a8b342535c836551076d24ad2d7cb
Irina Hristova <Irina.Hristova@experian.com>
2019-06-22 16:37:11 +0300

    refactor: heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {Hero} from '../hero'
 
 @Component({
-  selector: 'app-heroes',
+  //metadata properties
+  selector: 'app-heroes', 
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
@@ -12,4 +14,8 @@ export class HeroesComponent implements OnInit {
   ngOnInit() {
   }
 
+  hero: Hero = {
+    id: 1, 
+    name: 'Irina'
+  };
 }
